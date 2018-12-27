635faf8984114412e698b33aeced8c34fd3579bd
Gustavo Silva <gustavo.silva192@gmail.com>
2018-12-27 10:23:56 +0000

    Flash message Works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,18 @@
 module ApplicationHelper
-	def flash_message
-    messages = ""
-    [:notice, :info, :warning, :error].each {|type|
-      if flash[type]
-        messages += "<p class=\"#{type}\">#{flash[type]}</p>"
-      end
-    }
+	def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 
-    messages
-	end
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do 
+        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        concat message 
+      end)
+    end
+    nil
+  end
+  
 	def markdown(text)
     options = {
       prettify:        true,
